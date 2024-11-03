ed58f7b8829d7f53769a20a21b40d66bcb4d29c4
youlewazi <2713969797@qq.com>
2024-11-03 09:56:57 +0800

    项目暂时停滞

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import legacy from "@vitejs/plugin-legacy"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite';
@@ -17,7 +17,12 @@ export default defineConfig({
     Components({
       resolvers: [VantResolver()],
     }),
+    legacy({
+      targets: ["ie>=11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    })
   ],
+  base: './',
   server: {
     //使用IP能访问
       host: '0.0.0.0'
